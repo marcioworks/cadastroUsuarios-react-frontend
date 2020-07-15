456e54565341fc0f5ae0ffbe87115520e181898b
Marcio da Silva Santos <marcio.santos@fortbrasil.com.br>
2020-07-15 10:45:59 -0300

    frontend cadastro de usuarios

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Main from "../template/Main";
+
+export default props => 
+  <Main icon="home" title="Inicio" subtitle="Segundo projeto react">
+    <div className="display-4">Bem Vindo!</div>
+    <hr/>
+    <p className="mb-0">Sistema Para cadastro de Usuários em React</p>
+  </Main>
+
---
+++ src/components/template/Logo.jsx
@@ -0,0 +1,12 @@
+import './Logo.css'
+import React from 'react'
+import logo from   '../../assets/images/flower-logo.jpg'
+import {Link} from 'react-router-dom'
+
+
+export default props =>
+<aside className="logo">
+   <Link to="/" className="logo">
+       <img src={logo} alt="logo"/>
+   </Link>
+</aside>
